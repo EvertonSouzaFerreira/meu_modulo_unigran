a68912bdd869de744a1cd3d3ee53073864b91352
Everton Ferreira <everton.souzaferreira@outlook.com>
2022-05-22 18:15:54 +0200

    atualizando menssagem

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var colors = require ('colors');
 function ola(nome){
-	const texto1 = `Olá ${nome || 'web developer'}!`;
-	console.log(texto1);
+	const texto = `Bem vindo ${nome || 'a materia de desenvolvimento voltado a web 3'}!`.random;
+	console.log(texto);
 }
 
 exports.ola = ola;
---
+++ meu-projeto/testando.js
@@ -0,0 +1,2 @@
+const moduloAula01 = require('meu_modulo_unigran')
+console.log(moduloAula01('test'))
